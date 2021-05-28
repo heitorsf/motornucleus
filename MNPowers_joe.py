8d27894339f2102fc925c8006b193e0e569aa880
Heitor Fernandes <heitorsf@gmail.com>
2021-05-27 21:41:44 -0300

    Fix 'sectionless' variables in setMediumThreshMN()
    In the origina model some mechanism variables were set without
    link to any section. The fix map thoses variables and set values
    in all sections whick has the respective mechanism inserted.

--- MNPowers_joe.py
+++ MNPowers_joe.py
@@ -300,16 +300,61 @@ class FRMotoneuronNaHH(object):
         self.dend.taur_mAHP = 56.6666666666667
         self.dend.ghbar_gh = 0.000127777777777778
         self.dend.half_gh = -75
-        self.tmin_kdrRL = 0.8
-        self.taumax_kdrRL = 20
-        self.qinf_na3rp = 4.8
-        self.thinf_na3rp = -50.5
-        self.Rd_na3rp = 0.06
-        self.qd_na3rp = 1.3
-        self.qg_na3rp = 1.3
-        self.thi1_na3rp = -35
-        self.thi2_na3rp = -35
-        self.vslope_naps = 5
-        self.celsius = 37
-        self.theta_m_L_Ca = -43
-        self.V0 = -3.88888888888889
+
+        self.soma.tmin_kdrRL = 0.8
+        self.dend.tmin_kdrRL = 0.8
+        self.iseg.tmin_kdrRL = 0.8
+        self.axonhillock.tmin_kdrRL = 0.8
+
+        self.soma.taumax_kdrRL = 20
+        self.dend.taumax_kdrRL = 20
+        self.iseg.taumax_kdrRL = 20
+        self.axonhillock.taumax_kdrRL = 20
+
+        self.soma.qinf_na3rp = 4.8
+        self.dend.qinf_na3rp = 4.8
+        self.iseg.qinf_na3rp = 4.8
+        self.axonhillock.qinf_na3rp = 4.8
+
+        self.soma.thinf_na3rp = -50.5
+        self.dend.thinf_na3rp = -50.5
+        self.iseg.thinf_na3rp = -50.5
+        self.axonhillock.thinf_na3rp = -50.5
+
+        self.soma.Rd_na3rp = 0.06
+        self.dend.Rd_na3rp = 0.06
+        self.iseg.Rd_na3rp = 0.06
+        self.axonhillock.Rd_na3rp = 0.06
+
+        self.soma.qd_na3rp = 1.3
+        self.dend.qd_na3rp = 1.3
+        self.iseg.qd_na3rp = 1.3
+        self.axonhillock.qd_na3rp = 1.3
+
+        self.soma.qg_na3rp = 1.3
+        self.dend.qg_na3rp = 1.3
+        self.iseg.qg_na3rp = 1.3
+        self.axonhillock.qg_na3rp = 1.3
+
+        self.soma.thi1_na3rp = -35
+        self.dend.thi1_na3rp = -35
+        self.iseg.thi1_na3rp = -35
+        self.axonhillock.thi1_na3rp = -35
+
+        self.soma.thi2_na3rp = -35
+        self.dend.thi2_na3rp = -35
+        self.iseg.thi2_na3rp = -35
+        self.axonhillock.thi2_na3rp = -35
+
+        self.soma.vslope_naps = 5
+        self.dend.vslope_naps = 5
+        self.iseg.vslope_naps = 5
+        self.axonhillock.vslope_naps = 5
+
+        self.soma.theta_m_L_Ca = -43
+        self.dend.theta_m_L_Ca = -43
+
+        h.celsius = 37
+
+        # V0 is used for currents in the original model
+        #V0 = -3.88888888888889
