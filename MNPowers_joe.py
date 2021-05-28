812105b2ff505aaa9c2e25f3d57cbfea9e9026dc
Heitor Fernandes <heitorsf@gmail.com>
2021-05-27 21:09:14 -0300

    Convert setMediumThreshMN to use range_assignment

--- MNPowers_joe.py
+++ MNPowers_joe.py
@@ -171,95 +171,145 @@ class FRMotoneuronNaHH(object):
         #self.dend.gcabar_L_Ca(0.6:1)=0:0
         range_assignment(self.dend, var='gcabar_L_Ca', valrange=[0, 0], locrange=[0.6, 1.0])
 
-    
 
+    def setMediumTreshMN(self):
+        """As in: Medium_thresh_MN.hoc."""
+        ## SOMA
+        self.soma.diam = 53.8888888888889
+        self.soma.L = 53.8888888888889
+        self.soma.g_pas = 0.00291666666666667
+        self.soma.e_pas = -72
+        self.soma.gbar_na3rp = 0.044
+        self.soma.gbar_naps = 0.00044
+        self.soma.sh_na3rp = 5
+        self.soma.sh_naps = 15
+        self.soma.ar_na3rp = 0.4
+        self.soma.ar_naps = 0.4
+        self.soma.gMax_kdrRL = 0.07
+        self.soma.gcamax_mAHP = 8e-06
+        self.soma.gkcamax_mAHP = 0.0076
+        self.soma.taur_mAHP = 56.6666666666667
+        self.soma.ghbar_gh = 0.000127777777777778
+        self.soma.half_gh = -75
 
-    # def setMediumTreshMN(self):
-    #     """As in: Medium_thresh_MN.hoc."""
-    #     self.soma.diam = 53.8888888888889
-    #     self.soma.L = 53.8888888888889
-    #     self.soma.g_pas = 0.00291666666666667
-    #     self.soma.e_pas = -72
-    #     self.soma.gbar_na3rp = 0.044
-    #     self.soma.gbar_naps = 0.00044
-    #     self.soma.sh_na3rp = 5
-    #     self.soma.sh_naps = 15
-    #     self.soma.ar_na3rp = 0.4
-    #     self.soma.ar_naps = 0.4
-    #     self.soma.gMax_kdrRL = 0.07
-    #     self.soma.gcamax_mAHP = 8e-06
-    #     self.soma.gkcamax_mAHP = 0.0076
-    #     self.soma.taur_mAHP = 56.6666666666667
-    #     self.soma.ghbar_gh = 0.000127777777777778
-    #     self.soma.half_gh = -75
+        ## INITIAL SEGMENT
+        self.iseg.nseg = 9
+        self.iseg.diam = 3.84444444444444
+        self.iseg.g_pas = 0.001
+        self.iseg.e_pas = -72
+        self.iseg.gbar_na3rp = 0.638888888888889
+        self.iseg.gbar_naps = 0.0127777777777778
+        self.iseg.sh_na3rp = -3
+        self.iseg.sh_naps = 7
+        self.iseg.ar_na3rp = 0.4
+        self.iseg.ar_naps = 0.4
+        self.iseg.gMax_kdrRL = 0.4
 
-    #     self.iseg.nseg = 9
-    #     self.iseg.diam = 3.84444444444444
-    #     self.iseg.g_pas = 0.001
-    #     self.iseg.e_pas = -72
-    #     self.iseg.gbar_na3rp = 0.638888888888889
-    #     self.iseg.gbar_naps = 0.0127777777777778
-    #     self.iseg.sh_na3rp = -3
-    #     self.iseg.sh_naps = 7
-    #     self.iseg.ar_na3rp = 0.4
-    #     self.iseg.ar_naps = 0.4
-    #     self.iseg.gMax_kdrRL = 0.4
+        ## AXON HILLOK
+        self.axonhillock.nseg = 7
 
-    #     self.axonhillock.nseg = 7
-    #     self.axonhillock.diam(0:1) = 3.84444444444444:12.2222222222222
-    #     self.axonhillock.g_pas = 0.001
-    #     self.axonhillock.e_pas = -72
-    #     self.axonhillock.gbar_na3rp = 0.638888888888889
-    #     self.axonhillock.gbar_naps = 0.0127777777777778
-    #     self.axonhillock.sh_na3rp = -3
-    #     self.axonhillock.sh_naps = 7
-    #     self.axonhillock.ar_na3rp = 0.4
-    #     self.axonhillock.ar_naps = 0.4
-    #     self.axonhillock.gMax_kdrRL = 0.4
+        #self.axonhillock.diam(0:1) = 3.84444444444444:12.2222222222222
+        range_assignment(self.axonhillock, var='diam', locrange=[0,1], valrange=[3.84444444444444,12.2222222222222]
 
-    #     self.dend.nseg = 25
-    #     self.dend.L = 6422.22222222222
-    #     self.dend.diam(0:0.2) = 39.4444444444444:41.4444444444444
-    #     self.dend.diam(0.2:1) = 41.4444444444444:0
-    #     self.dend.g_pas = 7.22222222222222e-05
-    #     self.dend.e_pas = -72
-    #     self.dend.gbar_na3rp(0:0.04) = 0.044:0.044
-    #     self.dend.gbar_na3rp(0.04:1) = 0.00075:0.00075
-    #     self.dend.gbar_naps(0:0.04) = 0.00044:0.00044
-    #     self.dend.gbar_naps(0.04:1) = 1.5e-05:1.5e-05
-    #     self.dend.sh_na3rp = 5
-    #     self.dend.sh_naps = 15
-    #     self.dend.ar_na3rp(0:0.04) = 0.4:0.4
-    #     self.dend.ar_naps(0:0.04) = 0.4:0.4
-    #     self.dend.ar_na3rp(0.04:1) = 0.4:0.4
-    #     self.dend.ar_naps(0.04:1) = 0.4:0.4
-    #     self.dend.gMax_kdrRL(0:0.04) = 0.08:0.08
-    #     self.dend.gMax_kdrRL(0.04:1) = 0.00033:0.00033
-    #     self.dend.gcabar_L_Ca(0:0.32) = 0:0
-    #     self.dend.gcabar_L_Ca(0.32:0.56) = 0.00016:0.00016
-    #     self.dend.gcabar_L_Ca(0.56:1) = 0:0
-    #     self.dend.g_kca2(0:0.32) = 0:0
-    #     self.dend.g_kca2(0.32:0.56) = 4e-05:4e-05
-    #     self.dend.g_kca2(0.56:1) = 0:0
-    #     self.dend.depth2_kca2 = 200
-    #     self.dend.taur2_kca2 = 120
-    #     self.dend.gcamax_mAHP(0:0.04) = 8e-06:8e-06
-    #     self.dend.gcamax_mAHP(0.04:1) = 0:0
-    #     self.dend.gkcamax_mAHP(0:0.04) = 0.0038:0.0038
-    #     self.dend.gkcamax_mAHP(0.04:1) = 0:0
-    #     self.dend.taur_mAHP = 56.6666666666667
-    #     self.dend.ghbar_gh = 0.000127777777777778
-    #     self.dend.half_gh = -75
-    #     self.tmin_kdrRL = 0.8
-    #     self.taumax_kdrRL = 20
-    #     self.qinf_na3rp = 4.8
-    #     self.thinf_na3rp = -50.5
-    #     self.Rd_na3rp = 0.06
-    #     self.qd_na3rp = 1.3
-    #     self.qg_na3rp = 1.3
-    #     self.thi1_na3rp = -35
-    #     self.thi2_na3rp = -35
-    #     self.vslope_naps = 5
-    #     self.celsius = 37
-    #     self.theta_m_L_Ca = -43
-    #     self.V0 = -3.88888888888889
+        self.axonhillock.g_pas = 0.001
+        self.axonhillock.e_pas = -72
+        self.axonhillock.gbar_na3rp = 0.638888888888889
+        self.axonhillock.gbar_naps = 0.0127777777777778
+        self.axonhillock.sh_na3rp = -3
+        self.axonhillock.sh_naps = 7
+        self.axonhillock.ar_na3rp = 0.4
+        self.axonhillock.ar_naps = 0.4
+        self.axonhillock.gMax_kdrRL = 0.4
+
+        ## DENDRITE
+        self.dend.nseg = 25
+        self.dend.L = 6422.22222222222
+        self.dend.diam(0:0.2) = 39.4444444444444:41.4444444444444
+        range_assignment(self.dend, var='diam', locrange=[0,0.2], valrange[39.4444444444444,41.4444444444444]
+        self.dend.diam(0.2:1) = 41.4444444444444:0
+        range_assignment(self.dend, var='diam', locrange=[0.2,1], valrange[41.4444444444444,0]
+        self.dend.g_pas = 7.22222222222222e-05
+        self.dend.e_pas = -72
+        
+        #self.dend.gbar_na3rp(0:0.04) = 0.044:0.044
+        range_assignment(self.dend, var='gbar_na3rp', locrange=[0,0.04], valrange[0.044,0.044])
+        
+        #self.dend.gbar_na3rp(0.04:1) = 0.00075:0.00075
+        range_assignment(self.dend, var='gbar_na3rp', locrange=[0.04,1], valrange[0.00075,0.00075])
+        
+        #self.dend.gbar_naps(0:0.04) = 0.00044:0.00044
+        range_assignment(self.dend, var='gbar_naps', locrange=[0,0.04], valrange[0.00044,0.00044])
+
+        #self.dend.gbar_naps(0.04:1) = 1.5e-05:1.5e-05
+        range_assignment(self.dend, var='gbar_naps', locrange=[0.04,1], valrange[1.5e-05,1.5e-05])
+
+        self.dend.sh_na3rp = 5
+        self.dend.sh_naps = 15
+
+        #self.dend.ar_na3rp(0:0.04) = 0.4:0.4
+        range_assignment(self.dend, var='ar_na3rp', locrange=[0,0.04], valrange[0.4,0.4])
+
+        #self.dend.ar_naps(0:0.04) = 0.4:0.4
+        range_assignment(self.dend, var='ar_naps', locrange=[0,0.04], valrange[0.4,0.4])
+
+        #self.dend.ar_na3rp(0.04:1) = 0.4:0.4
+        range_assignment(self.dend, var='ar_na3rp', locrange=[0.04,1], valrange[0.4,0.4])
+
+        #self.dend.ar_naps(0.04:1) = 0.4:0.4
+        range_assignment(self.dend, var='ar_naps', locrange=[0.04,1], valrange[0.4,0.4])
+
+        #self.dend.gMax_kdrRL(0:0.04) = 0.08:0.08
+        range_assignment(self.dend, var='gMax_kdrRL', locrange=[0,0.04], valrange[0.08,0.08])
+
+        #self.dend.gMax_kdrRL(0.04:1) = 0.00033:0.00033
+        range_assignment(self.dend, var='gMax_kdrRL', locrange=[0.04,1], valrange[0.00033,0.00033])
+
+        #self.dend.gcabar_L_Ca(0:0.32) = 0:0
+        range_assignment(self.dend, var='gcabar_L_Ca', locrange=[0,0.32], valrange[0,0])
+
+        #self.dend.gcabar_L_Ca(0.32:0.56) = 0.00016:0.00016
+        range_assignment(self.dend, var='gcabar_L_Ca', locrange=[0.32,0.56], valrange[0.00016,0.00016])
+
+        #self.dend.gcabar_L_Ca(0.56:1) = 0:0
+        range_assignment(self.dend, var='gcabar_L_Ca', locrange=[0.56,1], valrange[0,0])
+
+        #self.dend.g_kca2(0:0.32) = 0:0
+        range_assignment(self.dend, var='g_kca2', locrange=[0,0.32], valrange[0,0])
+
+        #self.dend.g_kca2(0.32:0.56) = 4e-05:4e-05
+        range_assignment(self.dend, var='g_kca2', locrange=[0.32,0.56], valrange[4e-05,4e-05])
+
+        #self.dend.g_kca2(0.56:1) = 0:0
+        range_assignment(self.dend, var='g_kca2', locrange=[0.56,1], valrange[0,0])
+
+        self.dend.depth2_kca2 = 200
+        self.dend.taur2_kca2 = 120
+
+        #self.dend.gcamax_mAHP(0:0.04) = 8e-06:8e-06
+        range_assignment(self.dend, var='gcamax_mAHP', locrange=[0,0.04], valrange[8e-06,8e-06])
+
+        #self.dend.gcamax_mAHP(0.04:1) = 0:0
+        range_assignment(self.dend, var='gcamax_mAHP', locrange=[0.04,1], valrange[0,0])
+
+        #self.dend.gkcamax_mAHP(0:0.04) = 0.0038:0.0038
+        range_assignment(self.dend, var='gkcamax_mAHP', locrange=[0,0.04], valrange[0.0038,0.0038])
+
+        #self.dend.gkcamax_mAHP(0.04:1) = 0:0
+        range_assignment(self.dend, var='gkcamax_mAHP', locrange=[0.04,1], valrange[0,0])
+
+        self.dend.taur_mAHP = 56.6666666666667
+        self.dend.ghbar_gh = 0.000127777777777778
+        self.dend.half_gh = -75
+        self.tmin_kdrRL = 0.8
+        self.taumax_kdrRL = 20
+        self.qinf_na3rp = 4.8
+        self.thinf_na3rp = -50.5
+        self.Rd_na3rp = 0.06
+        self.qd_na3rp = 1.3
+        self.qg_na3rp = 1.3
+        self.thi1_na3rp = -35
+        self.thi2_na3rp = -35
+        self.vslope_naps = 5
+        self.celsius = 37
+        self.theta_m_L_Ca = -43
+        self.V0 = -3.88888888888889
